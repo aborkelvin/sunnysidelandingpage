9236ae772f84ea9697c96b69e9a437fd2b4d2324
Kelvin <aborkelvin@gmail.com>
2022-06-23 01:35:00 +0100

    Added the function to display/hide nav bar onclick of nav-icon

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let headnav = document.querySelector('.headnav');
+function displaynav(){
+    if(headnav.style.display == 'none'){
+        headnav.style.display = 'block';
+    }else{
+        headnav.style.display = 'none'
+    }
+
+}
