867ad104b06b125820315749530df84bd13856b7
Kelvin <aborkelvin@gmail.com>
2022-06-23 11:07:37 +0100

    Updating the nav dropdown menu with a better functionality

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 
 let headnav = document.querySelector('.headnav');
 function displaynav(){
-    if(headnav.style.display == 'none'){
-        headnav.style.display = 'block';
-    }else{
-        headnav.style.display = 'none'
-    }
-
+    headnav.classList.toggle('show')
 }
